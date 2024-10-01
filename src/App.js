9eb6c2906302b89032ed7fae2cce7faf3b7aec4a
ManishBailwal <102870593+ManishBailwal@users.noreply.github.com>
2024-10-01 17:54:10 +0530

    pushing code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import Hero from './components/Hero/Hero'
 import './App.css';
+import AllProduct from './components/AllProduct/AllProduct';
+import MainProductSection from './components/MainProductSection/MainProductSection';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hero/>
+     <AllProduct/>
+     <MainProductSection/>
+
+     <div id="footer"></div>
+
+      
     </div>
   );
 }
